49c8512eeaafaf7ccaebcaaf607cca0fb2fc6082
Anton Lavrov <anton.lavrov@oracle.com>
2022-12-06 08:37:39 -0800

    day 4

---
+++ 04.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+(define (make-pair s)
+  (match-define (list assignment1 assignment2) (string-split s ","))
+  (list (make-assignment assignment1) (make-assignment assignment2))
+)
+
+(define (make-assignment s)
+  (map string->number (string-split s "-"))
+)
+
+(define (full-overlap? a1 a2)
+  (match-define (list b1 e1) a1)
+  (match-define (list b2 e2) a2)
+  (or (= b1 b2) (if (< b1 b2) (>= e1 e2) (<= e1 e2)))
+)
+
+(define (overlap? a1 a2)
+  (match-define (list b1 e1) a1)
+  (match-define (list b2 e2) a2)
+  (or (and (>= e1 b2) (<= b1 e2)) (and (>= e2 b1) (<= b2 e1)))
+)
+
+(define pairs (map make-pair (file->lines "04.input")))
+
+(define overlaps1 (filter (curry apply full-overlap?) pairs))
+(println (length overlaps1))
+
+(define overlaps2 (filter (curry apply overlap?) pairs))
+(println (length overlaps2))
