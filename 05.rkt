7653f0f9467038c9fd694b7bc24eb56e642e3a0f
Anton Lavrov <anton.lavrov@oracle.com>
2022-12-08 20:59:57 -0800

    day 5

---
+++ 05.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+(define (parse-crates crates)
+  (define transposed (apply map list (map string->list crates)))
+  (define lines-stack (filter (lambda (l) (not-space (car l))) transposed))
+  (define stacks (map (lambda (l) (reverse (takef (cdr l) not-space))) lines-stack))
+  stacks
+)
+
+(define (not-space c)
+  (not (equal? #\space c))
+)
+
+(define (parse-move s)
+  (define ts (string-split s))
+  (map string->number (list (list-ref ts 1) (list-ref ts 3) (list-ref ts 5)))
+)
+
+(define (make-move bulk? move crates)
+  (define-values (n index-from index-to) (apply values move))
+
+  (define stack-from (list-ref crates (- index-from 1)))
+  (define stack-from-new (drop stack-from n))
+
+  (define crates-to-move (take stack-from n))
+
+  (define stack-to (list-ref crates (- index-to 1)))
+  (define stack-to-new (append (if bulk? crates-to-move (reverse crates-to-move)) stack-to))
+
+  (define crates1 (list-set crates (- index-from 1) stack-from-new))
+  (define crates2 (list-set crates1 (- index-to 1) stack-to-new))
+  crates2
+)
+
+(define (tops crates)
+  (list->string (map car crates))
+)
+
+
+(define-values (lines-crates lines-moves) (splitf-at (file->lines "05.input") non-empty-string?))
+(define crates (parse-crates (reverse lines-crates)))
+;(println crates)
+(define moves (map parse-move (cdr lines-moves)))
+;(println moves)
+
+(define tops1 (tops (foldl (curry make-move #f) crates moves)))
+(println tops1)
+
+(define tops2 (tops (foldl (curry make-move #t) crates moves)))
+(println tops2)
