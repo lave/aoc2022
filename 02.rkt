6a919f8cc85447cc10be5791e75b94821e0722d8
Anton Lavrov <anton.lavrov@oracle.com>
2022-12-10 09:27:06 -0800

    refactored day 2

--- 02.rkt
+++ 02.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
 (define (weight shape)
-  (case shape [("A" "X") 0] [("B" "Y") 1] [("C" "Z") 2])
+  (case shape
+    [("A" "X") 0]
+    [("B" "Y") 1]
+    [("C" "Z") 2])
 )
 
 (define (score opp you)
@@ -15,19 +18,21 @@
   (score (weight opp) (weight you))
 )
 
-(define (find-shape outcome w)
-  (case outcome [("X") (modulo (- w 1) 3)] [("Y") w] [("Z") (modulo (+ w 1) 3)]))
-
 (define (score2 opp outcome)
   (define w (weight opp))
-  (score w (find-shape outcome w))
+  (define you (case outcome
+    [("X") (modulo (- w 1) 3)]
+    [("Y") w]
+    [("Z") (modulo (+ w 1) 3)]))
+  (score w you)
 )
 
 
 (define rounds (map string-split (file->lines "02.input")))
+;(println rounds)
 
 (define total1 (foldl + 0 (map (curry apply score1) rounds)))
-(println total1)
+(displayln total1)
 
 (define total2 (foldl + 0 (map (curry apply score2) rounds)))
-(println total2)
+(displayln total2)
