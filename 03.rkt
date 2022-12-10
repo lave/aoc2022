d7b3dd16f5854a16e73cef6841a5eed52d44a770
Anton Lavrov <anton.lavrov@oracle.com>
2022-12-10 10:38:23 -0800

    refactored day 3

--- 03.rkt
+++ 03.rkt
@@ -9,12 +9,12 @@
   (if (not (empty? lst)) (cons (take lst n) (split-by (drop lst n) n)) '())
 )
 
-(define (weight item)
+(define (priority item)
   (define c (char->integer item))
   (if (< c 97) (- c 38) (- c 96))
 )
 
-(define (find-dup l r)
+(define (find-duplicate l r)
   (findf (lambda (c) (string-contains? l (list->string (list c)))) (string->list r))
 )
 
@@ -26,9 +26,9 @@
 (define lines (file->lines "03.input"))
 
 (define rucksacks (map split-middle lines))
-(define dups (map (curry apply find-dup) rucksacks))
-(println (apply + (map weight dups)))
+(define duplicates (map (curry apply find-duplicate) rucksacks))
+(displayln (apply + (map priority duplicates)))
 
 (define groups (split-by lines 3))
 (define badges (map (curry apply find-badge) groups))
-(println (apply + (map weight badges)))
+(displayln (apply + (map priority badges)))
